cb3f3c8238120b47c42b88521b37a50812e56dbc
Hjalte Mann <hjalte@me.com>
2021-12-02 11:57:08 +0100

    Shows more info on screen. Runs on sorted list. Kills if no images.

--- code/ShortcutSorting.py
+++ code/ShortcutSorting.py
@@ -11,16 +11,14 @@ import sys
 import shutil 
 import ast
 
-
 ####
 ## USER SETTINGS
 ####
 
-
 directory = r'../images/ShortcutSorting' # Path to folder with images for sorting
-
-
 folderMap = {'1': "Empty", '2': "Maybe", '3': "Insect"}
+
+img_w, img_h = 1140, 1000
 
 ####
 ## HARD SETTINGS
@@ -34,17 +32,17 @@ width, height = info.current_w, info.current_h
 imageDisplay_width, imageDisplay_height = 1040, 1040 #int(width/2), int(height/2)
 
 imageDisplay = pygame.display.set_mode((0,0), pygame.FULLSCREEN)# (imageDisplay_width,imageDisplay_height), pygame.RESIZABLE)
-#pygame.display.set_caption('Fast manual sorting')
+pygame.display.set_caption('Fast manual sorting')
 
 #x, y =  (imageDisplay_width * 0.15), (imageDisplay_height * 0.2)
-x, y =  600, 40 # Coordinates for image position in program window
-
+x, y =  350, 40 # Coordinates for image position in program window
 
 #####
 ## DEFINE SOME FUNCTIONS
 #####
 
-pp = (os.path.join(directory, file) for file in os.listdir(directory) if file.endswith((".jpg", ".JPG", ".png", ".PNG"))) # Generator for feeding images
+imgList = sorted([file for file in os.listdir(directory) if file.endswith((".jpg", ".JPG", ".png", ".PNG"))])
+pp = (os.path.join(directory, file) for file in imgList) # Generator for feeding images
 
 def displayImage(img, x,y):
     imageDisplay.blit(img, (x,y))
@@ -61,7 +59,8 @@ def createFolders(folderMap):
 def copyImage(keyPressed, image):
     src = image
     dst = os.path.join(directory, folderMap[keyPressed])
-    shutil.copy(src, dst)
+    shutil.move(src, dst)
+    print("Moved to: ", folderMap[keyPressed])
  
 def waitKeypress():
     wait = True
@@ -77,28 +76,46 @@ def waitKeypress():
                 return pygame.key.name(event.key)
                 break
 
+def showInfo(infoText):
+    info = fontSmall.render(infoText, True, (255, 255, 255), (0, 0, 0)) # create a text surface object, on which text is drawn on it.
+    infoRect = info.get_rect()# create a rectangular object for the text surface object
+    infoRect.center = (x // 2, y // 2) # set the center of the rectangular object.
+    return info
+
+
 # Stuff for displaying folder info text
 font = pygame.font.Font('freesansbold.ttf', 32)
+fontSmall = pygame.font.Font('freesansbold.ttf', 16)
 text = font.render(str(folderMap).replace('{', '').replace(',', '').replace('}', '').replace("'", ''), True, (255, 255, 255), (0, 0, 0)) # create a text surface object, on which text is drawn on it.
 textRect = text.get_rect()# create a rectangular object for the text surface object
 textRect.center = (x // 2, y // 2) # set the center of the rectangular object.
 
+
+if not imgList: # If no images in directory, kill program.
+    print("No images in directory. Killed program.")
+    kill()
+
 def run():
     """ The main program"""
     createFolders(folderMap) # Create the folders
-    image = next(pp) # Grab the first image from the generator      
+    left = len(imgList) + 1
+    image = next(pp) # Grab the first image from the generator    
     while True:     
         displayImage(text, 0,0)
+        displayImage(showInfo(image.split('\\')[-1]), 0,40)
+        left -= 1
+        displayImage(showInfo(f'Images left: {left}'), 0,80)
         
+        print(image)  
         picture = pygame.image.load(image)
-        picture = pygame.transform.scale(picture, (1400, 1400))
+        picture = pygame.transform.scale(picture, (img_w, img_h))
         
         displayImage(picture, x, y)
         pygame.display.update()  
         keyPressed = waitKeypress()
         keyPressed = str(ast.literal_eval(keyPressed)[0]) # Key 1 returns '1' but numpad 1 returns '[1]' so we'll check if the string is a list, if so we'll grab first (and only) instance and convert to string.
         copyImage(keyPressed, image)
-
+        
         try:
             image = next(pp) # Grab the next image from the generator
         except StopIteration: # We'll catch the StopIteration from the generator when it has no more images to feed and kill the program.
